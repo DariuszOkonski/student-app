3a9e8ba2843f6d071fb9a9303f24a48c72f00167
Dariusz Okonski <darek200180@gmail.com>
2021-10-27 02:13:56 +0100

    add class Student

--- src/main/java/com/example/studentapp/StudentAppApplication.java
+++ src/main/java/com/example/studentapp/StudentAppApplication.java
@@ -2,6 +2,10 @@ package com.example.studentapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 public class StudentAppApplication {
@@ -9,5 +13,4 @@ public class StudentAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StudentAppApplication.class, args);
 	}
-
 }
