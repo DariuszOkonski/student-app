fbec79a2b019b9552c3f6db858771a745dee04aa
Dariusz Okonski <darek200180@gmail.com>
2021-10-27 02:34:13 +0100

    next Properties File, connection to db

--- src/main/java/com/example/studentapp/student/StudentController.java
+++ src/main/java/com/example/studentapp/student/StudentController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+// Properties File
+
 @RestController
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
